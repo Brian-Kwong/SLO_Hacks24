5c222a7c7311ad9ff717d0178c6772331c80dc61
tammy-si <sitammy6@gmail.com>
2024-05-25 13:34:21 -0700

    Added ingredients field and desc field

--- packages/ReverseFood/components/FoodInput.js
+++ packages/ReverseFood/components/FoodInput.js
@@ -14,9 +14,11 @@ import * as ImagePicker from "expo-image-picker";
 export default function FoodInput() {
   const [image, setImage] = useState(null);
   const [foodName, setFoodName] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [desc, setDescription] = useState("");
 
+  // uses Image picker to get an image from user's library
   const pickImage = async () => {
-    console.log("herello");
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -29,16 +31,19 @@ export default function FoodInput() {
     }
   };
 
+  // To handle submission of form. probably fetch here
   const handleSubmit = () => {
     console.log(image);
     console.log(foodName);
+    console.log(ingredients);
+    console.log(desc);
   };
 
   return (
     <View>
       <Text>Name of food:</Text>
       <TextInput
-        placeholder="test"
+        placeholder="Name"
         onChangeText={(text) => setFoodName(text)}
         value={foodName}
       ></TextInput>
@@ -47,8 +52,18 @@ export default function FoodInput() {
       {image && (
         <Image source={{ uri: image }} style={{ height: 300, width: 300 }} />
       )}
-      <TextInput></TextInput>
       <Text>Ingredients:</Text>
+      <TextInput
+        placeholder="Ingredients"
+        onChangeText={(text) => setIngredients(text)}
+        value={ingredients}
+      ></TextInput>
+      <Text>Description:</Text>
+      <TextInput
+        placeholder="Description"
+        onChangeText={(text) => setDescription(text)}
+        value={desc}
+      ></TextInput>
       <Button title="Submit" onPress={handleSubmit} />
     </View>
   );

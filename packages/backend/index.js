8c53a23dd4f4a5ea8c90d1fed5e47cfc8bf1f08d
Kevin <kev.yuan01@gmail.com>
2024-05-25 23:59:26 -0700

    Reorganized routes

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -8,20 +8,17 @@ import { addAuthRoutes } from './authroutes.js';
 const app = express();
 const router = express.Router();
 // Add json middleware
-app.use(express.json({ limit: "50mb"}));
+app.use(express.json({limit: "50mb"}));
+
 addRoutes(app);
 addAuthRoutes(router);
 const storage = multer.memoryStorage();
 const multerServer = multer({ storage: storage });
 
-app.post("/image", postImage);
-app.get("/images");
-app.get("/foodFacts", getFoodFacts);
 app.use("/auth", router);
 
 // Connect to mongo DB
 connect("test");
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
